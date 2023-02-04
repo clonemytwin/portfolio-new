1fca5d83eeba645dfbab225f6d27bc08a8475750
Mohammad Wasi <wasihey8@gmail.com>
2023-02-04 13:42:46 +0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ function Home() {
                   <div>
                     <h1 className="m-0 font-big slide-in-top">Hi,</h1>
                     <h1 className="m-0 font-big animate-charcter">I'm Wasi,</h1>
-                    <p className="ls-2 mt-2 slide-in-bottom">Full Stack Developer / Computer Engineer / Artist</p>
+                    <p className="ls-2 mt-2 slide-in-bottom">Frontend React Developer / Computer Engineer / Artist</p>
                   </div>
                 </div>
 
